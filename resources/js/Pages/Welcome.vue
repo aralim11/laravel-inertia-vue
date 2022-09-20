5b24ec320bd5c5f2121107b06f9bd8f45614fe9c
Md. Abdul Alim <aralim11@gmail.com>
2022-09-21 01:56:13 +0600

    Laravel Inertia Vue 3 BoilerPlate

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>{{ data }}</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            data: "Hello World",
+        }
+    }
+}
+</script>
